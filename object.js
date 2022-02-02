c71ccad46e2f6602878b17dd14dafa4903a50191
vaansuit <vvansuit@outlook.com>
2022-02-02 19:17:06 -0300

    commit

---
+++ object.js
@@ -0,0 +1,69 @@
+const cliente = {
+    nome: 'Victor',
+    idade: 27,
+    cpf: "00000000000",
+    email: "email@email"
+}
+
+console.log(cliente.nome)
+console.log(cliente.idade)
+
+const produto = {
+    desc: 'FILTRO DE OLEO',
+    ean: '789789789789789798',
+    preco: 19.90,
+    est: 'positivo'
+}
+
+console.log(produto.desc)
+console.log(produto.preco)
+console.log(produto.est)
+
+const modulo = {
+    ativo: true,
+    cnf: false,
+    code: 1111.222,
+    state: 'Não configurado'
+}
+
+console.log(modulo.ativo)
+console.log(modulo.state)
+console.log(modulo.code)
+
+const youtube = {
+    viciado: 'sim',
+    horasDia: 24,
+    var: 'x',
+}
+
+console.log(youtube.viciado)
+console.log(youtube.horasDia)
+
+const home = {
+    servidor: 'bugado',
+    ativo: false,
+    empresa: 'lixo'
+}
+ console.log(home.ativo)
+ console.log(home.empresa)
+
+ const binario = {
+     um: 1,
+     zero: 0,
+     outros: 'NaN'
+ }
+
+ console.log(binario.um)
+ console.log(binario.zero)
+
+ const treino = {
+     repetir: true,
+     praticar: true,
+     procrastinar: false, 
+     consistencia: true
+ }
+
+ console.log(treino.repetir)
+ console.log(treino.praticar)
+ console.log(treino.procrastinar)
+ 
