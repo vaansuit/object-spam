d759f712c36a7ab2bff9a7e0b64854d7e41dfa41
vaansuit <vvansuit@outlook.com>
2022-02-09 19:24:26 -0300

    selecting data

---
+++ type-of-data-values.js
@@ -0,0 +1,48 @@
+//Colecting info
+
+const user = {
+    name: 'Joao',
+    age: 24,
+    cpf: '00000000000',
+    email: 'user@gmail.com',
+    fone:['123123123123', '12312312312']
+}
+
+const user2 = {
+    name: 'Paulo',
+    age: 23, 
+    cpf: '12312312300',
+    email: 'user@gmail.com',
+    fone:['322323232323', '121212121212']
+}
+
+const user3 = {
+    name: 'Justin',
+    age: 30,
+    cpf: '12345678900',
+    email: 'user3@gmail.com',
+    fone:['434343434343', '343434343434']
+}
+
+const user4 = {
+    name: 'Bill',
+    age: 45,
+    cpf: '32165498700',
+    email: 'user4@gmail.com',
+    fone:['676767676767', '767676767676']
+}
+
+const user5 = {
+    name: 'Flick',
+    age: 27,
+    cpf: '44455566600',
+    email: 'user5@gmail.com',
+    fone:['909090909090', '090909090909']
+}
+
+user.fone.forEach(fone => console.log(fone))
+user2.fone.forEach(fone => console.log(fone))
+user3.fone.forEach(fone => console.log(fone))
+user3.fone.forEach(fone => console.log(fone))
+user4.fone.forEach(fone => console.log(fone))
+user5.fone.forEach(fone => console.log(fone))
