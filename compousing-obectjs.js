98d521cc0dcec9f23832f4f6fdc1a8b09fab40dd
vaansuit <vvansuit@outlook.com>
2022-02-10 12:42:34 -0300

    added compousing-objects.js

---
+++ compousing-obectjs.js
@@ -0,0 +1,149 @@
+const char = {
+    name: 'Rod',
+    health: 90,
+    powerAtk: 100,
+    powerDef: 30,
+        
+}
+console.log(char)
+
+char.weapons = {
+    weapons: ['Axe', 'Two Hands Mace', 'Two Hands Shield']
+}
+
+console.log(char);
+
+const char2 = {
+    name: 'Dix',
+    health: 70, 
+    powerAtk: 45,
+    powerDef: 175,
+}
+
+console.log(char2)
+
+char2.weapons = {
+    weapons: ['Wand', 'Lamp', 'Power Staff']
+}
+
+// didnt worked lol console.log(char2.weapons[0])
+
+console.log(char2.weapons)
+
+const char3 = {
+    name: 'Elf',
+    health: 65,
+    powerAtk: 65,
+    powerDef: 65,
+}
+
+console.log(char3)
+
+char3.weapons = {
+    weapons: ['Gun', 'Arrow', 'Spear']
+}
+
+console.log(char3)
+
+const char4 = {
+    name: 'Joe',
+    health: 175,
+    powerAtk: 100,
+    powerDef: 80,
+}
+
+console.log(char4)
+
+char4.weapons = {
+    weapons: ['Gloves', 'Knifes']
+}
+
+console.log(char4)
+
+const char5 = {
+    name: 'Destiny',
+    health: 150,
+    powerAtk: 100,
+    powerDef: 250,
+}
+
+console.log(char5)
+
+char5.weapons = {
+    weapons: ['Wand', 'Guns', 'Arrow']
+}
+
+console.log(char5)
+
+const char6 = {
+    name: 'George',
+    health: 70,
+    powerAtk: 70,
+    powerDef: 80,
+}
+
+console.log(char6)
+
+char6.weapons = {
+    weapons: ['Wand', 'Staff']
+}
+
+console.log(char6)
+
+const char7 = {
+    name: 'Alan',
+    health: 89,
+    powerAtk: 78,
+    powerDef: 98
+}
+
+console.log(char7)
+
+char7.weapons = {
+    weapons: ['Spear', 'Guns', 'Gloves']
+}
+ console.log(char7)
+
+ const char8 = {
+     name: 'Sara',
+     health: 111,
+     powerAtk: 321,
+     powerDef: 665
+ }
+ console.log(char7)
+
+ char8.weapons = {
+     weapons: ['Gloves', 'Staff', 'Book']
+ }
+
+ console.log(char8)
+
+ const char9 = {
+     name: 'Mage',
+     health: 2500,
+     powerAtk: 2500,
+     powerDef: 3000
+ }
+
+ console.log(char9)
+
+ char9.weapons = {
+     weapons: ['Staff', 'Books']
+ }
+
+ console.log(char9)
+
+ const char10 = {
+     name: 'Final Boss',
+     health: 'Infinite',
+     powerAtk:'Infinite',
+     powerDef: 'Infinite'
+ }
+
+ console.log(char10)
+
+ char10.weapons = {
+     weapons: 'Nothing'
+ }
+
+ console.log(char10)
