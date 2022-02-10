70799d604aba4e6b525a39893bd023c631b29a75
vaansuit <vvansuit@outlook.com>
2022-02-10 15:21:35 -0300

    added object-list

---
+++ object-list.js
@@ -0,0 +1,97 @@
+const elm = {
+    type: 'Water',
+    powerAtk: 100,
+    powerDef:100,
+    monsters:  [{
+        name: 'Abstron',
+        age: 700
+    }]
+}
+
+console.log(elm)
+
+elm.monsters.push({name: 'Brubstorn', age: 320})
+
+console.log(elm)
+
+const elm2 = {
+    type: 'Fire',
+    powerAtk: 1000,
+    powerDef: 500,
+    monsters: [{
+        name: 'Cliverston',
+        age: 44
+    }]
+}
+
+console.log(elm2)
+
+elm.monsters.push({name: 'Dwight', age: 32})
+
+console.log(elm2)
+
+const elm3 = {
+    type: 'Leaf',
+    powerAtk: 789,
+    powerDef: 567,
+    monsters: [{
+        name: 'Elfstorn',
+        age: 909,
+    }]
+}
+
+console.log(elm3)
+
+elm3.monsters.push({name: 'Fugstion', age: 876})
+
+console.log(elm3)
+
+
+const elm4 = {
+    type: 'Wood',
+    powerAtk: 234,
+    powerDef: 32123,
+    monsters: [{
+        name: 'Gringoot',
+        age: 3421
+    }]
+}
+
+console.log(elm4)
+
+elm4.monsters.push({name: 'Hursra', age: 12314})
+
+console.log(elm4)
+
+const elm5 = {
+    type: 'Dust',
+    powerAtk: 123123,
+    powerDef: 3123213,
+    monsters: [{
+        name: 'Inasdask',
+        age: 13324
+    }]
+}
+
+console.log(elm5)
+
+elm5.monsters.push({name: 'Jugernaut', age: 6456})
+
+console.log(elm5)
+
+const elm6 = {
+    type: 'Spirit',
+    powerAtk: 123124,
+    powerDef: 43234,
+    monsters: [{
+        name: 'Kasldfkf',
+        age: 12312412
+    }]
+}
+
+console.log(elm6)
+
+elm6.monsters.push({name: 'Lrasra', age: 123123})
+
+console.log(elm6)
+
